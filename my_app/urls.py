517a7b635d58d1cc3a6331eb5485a8644bab8b36
antonella.caroca <antonella.caroca@cci-entel.cl>
2020-03-03 17:30:16 -0300

    part 2 simple upload ok

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,9 +3,13 @@ from . import views
 from .views import (
     Home,
     upload,
+    book_list,
+    upload_book,
 )
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('upload/', views.upload, name='upload'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/upload', views.upload_book, name='upload_book'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
+
+from .forms import BookForm
+from .models import Book
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -13,4 +16,22 @@ def upload(request):
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
     return render(request, 'upload.html', context)
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {
+        'books': books
+    })
+
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm() # instance of the form
+    return render(request, 'upload_book.html', {
+        'form':form
+    })
     
