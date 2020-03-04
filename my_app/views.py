016160015e3992ffc3bb8388602f79e5bcb2e269
antonella.caroca <antonella.caroca@cci-entel.cl>
2020-03-04 10:33:32 -0300

    parte 4 simple upload ok

--- my_app/views.py
+++ my_app/views.py
@@ -35,6 +35,12 @@ def upload_book(request):
     return render(request, 'upload_book.html', {
         'form':form
     })
+
+def delete_book(request, pk):
+    if request.method == 'POST':
+        book = Book.objects.get(pk=pk)
+        book.delete()
+    return redirect('book_list')
     
 class BookListView(ListView):
     model = Book
